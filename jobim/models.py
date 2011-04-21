c50619213c8aaf71a30be3884dfda8c3030c2868
Danilo Cabello <danilo.cabello@gmail.com>
2011-04-21 01:36:23 -0300

    Translate Category model

--- jobim/models.py
+++ jobim/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, verbose_name='nome')
+    name = models.CharField(max_length=50)
     slug = models.CharField(max_length=50)
 
     class Meta:
-        verbose_name = 'categoria'
+        verbose_name_plural = 'categories'
 
     def __unicode__(self):
         return self.name
