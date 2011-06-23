af29cbb0fb0bacbe24ed3fe68c1fb8285fd95001
Danilo Cabello <danilo.cabello@gmail.com>
2011-06-23 01:32:01 -0300

    Fix test_helpers to get tests green

--- jobim/tests/helpers.py
+++ jobim/tests/helpers.py
@@ -1,8 +1,22 @@
+def add_test_store():
+    from jobim.models import Store
+
+    store = Store(
+        name='Manoel Cowboy',
+        slogan='Manoel is selling everything! Enjoy.',
+        url='manoel')
+    store.save()
+
+    return store
+
 def add_test_product():
     from jobim.models import Product, Category
 
+    store = add_test_store()
+
     category = Category.objects.get(slug='books')
     product = Product(
+        store=store,
         name='The Pragmatic Programmer',
         slug='pragmatic-programmer',
         description='From Journeyman to master',
