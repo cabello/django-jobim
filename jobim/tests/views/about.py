5fd0d7cb41f9f4b248c01f75409f41b3f30a5b95
Danilo Cabello <danilo.cabello@gmail.com>
2011-10-18 15:55:04 -0200

    Rename about view test

--- jobim/tests/views/about.py
+++ jobim/tests/views/about.py
@@ -5,7 +5,7 @@ from jobim.tests.helpers import ViewTestCase
 
 class AboutViewTest(ViewTestCase):
 
-    def test_about(self):
+    def test_uses_right_template(self):
         response = self.client.get(reverse('jobim:about', **self.url_kwargs))
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'jobim/about.html')
